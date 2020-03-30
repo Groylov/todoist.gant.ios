ce3f104381fae611efbe06fe1507b52daa358a9b
Groylov Aleksey <groylov@gmail.com>
2020-03-31 01:52:08 +0900

    Initial Commit

---
+++ todoist.gant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todoist.gant
+//
+//  Created by Aleksey Groylov on 31.03.2020.
+//  Copyright © 2020 Aleksey Groylov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
